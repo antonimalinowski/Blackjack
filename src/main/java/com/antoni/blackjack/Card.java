8aa3f9200877b2dcfa96e9ee779d38ba71ce6ba5
antek <antonimalinowski.kontakt@gmail.com>
2022-02-20 21:23:43 +0100

    Boilerplate code, use Enums

---
+++ src/main/java/com/antoni/blackjack/Card.java
@@ -0,0 +1,37 @@
+package com.antoni.blackjack;
+
+public class Card {
+    private Suit suit;
+    private Rank rank;
+
+    public Card(Suit suit, Rank rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public static void main(String[] args) {
+        Card card1 = new Card(Suit.SPADES, Rank.ACE);
+        Card card2 = new Card(Suit.DIAMONDS, Rank.TEN);
+        // int total = card1.plus(card2);
+        // Hand myHand = new Hand();
+        // myHand.add(card1);
+        // myHand.add(card2);
+        // int total = myHand.getTotal();
+    }
+}
