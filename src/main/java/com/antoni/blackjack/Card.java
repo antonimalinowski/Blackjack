369fe27c450719e007dee66b109752fa6be41660
antek <antonimalinowski.kontakt@gmail.com>
2022-02-21 22:12:54 +0100

    Print a card to the console

--- src/main/java/com/antoni/blackjack/Card.java
+++ src/main/java/com/antoni/blackjack/Card.java
@@ -9,6 +9,11 @@ public class Card {
         this.rank = rank;
     }
 
+    public Card(String suit, String rank) {
+        this.suit = Suit.valueOf(suit.toUpperCase());
+        this.rank = Rank.valueOf(rank.toUpperCase());
+    }
+
     public Suit getSuit() {
         return suit;
     }
@@ -28,11 +33,22 @@ public class Card {
     public static void main(String[] args) {
         Card card1 = new Card(Suit.CLUBS, Rank.JACK);
         Card card2 = new Card(Suit.DIAMONDS, Rank.TEN);
+        Card card3 = new Card("hearts", "queen");
+
+        System.out.println(card1);
+        System.out.println(card2);
+        System.out.println(card3);
         // int total = card1.plus(card2);
         // Hand myHand = new Hand();
         // myHand.add(card1);
         // myHand.add(card2);
         // int total = myHand.getTotal();
+    }
+
+    @Override
+    public String toString() {
+        // JACK (Club symbol)
+        return this.rank.toString().concat(" ").concat(this.suit.toString());
     }
 
     public int getValue() {
