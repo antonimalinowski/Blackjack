a54ade7f5e4d865c239a820c4f580276a30c0751
antek <antonimalinowski.kontakt@gmail.com>
2022-02-21 21:49:07 +0100

    Add test for suit symbol, unicode character

---
+++ src/test/java/com/antoni/blackjack/SuitTest.java
@@ -0,0 +1,12 @@
+package com.antoni.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuitTest {
+    @Test
+    void heartPrintsHeart() {
+        assertEquals("\u2665", Suit.HEARTS.toString());
+    }
+}
